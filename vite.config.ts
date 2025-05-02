11fdd7fdf3ec50b665765a0ebbb400345d2c0aa2
jaeyoung.heo <gjwodud119@gmail.com>
2025-05-02 14:39:32 +0900

    Added github-blog optimized options

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jae-heo.github.io/', // 'react-blog'는 GitHub 저장소 이름. 필요에 따라 변경하세요.
+  base: '/',
 })
